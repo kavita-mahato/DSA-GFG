4d0b238718b69d2da46db34d1df96831f1282a7b
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-13 21:47:41 +0530

    Reverse Bits

---
+++ S03_Bit_Magic/reverseBits.java
@@ -0,0 +1,14 @@
+package S03_Bit_Magic;
+
+public class reverseBits {
+    static Long reversedBits(Long x) {
+        // code here
+        long reversed = 0;
+        for (int i = 0; i < 32; i++) {
+            long bit = x & 1;
+            reversed = (reversed << 1) | bit;
+            x >>= 1;
+        }
+        return reversed;
+    }
+}
