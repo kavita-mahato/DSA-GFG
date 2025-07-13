7d5a258170c062093cecc5992309715211abd398
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-13 21:45:27 +0530

    Game with nos

---
+++ S03_Bit_Magic/gameWithNumber.java
@@ -0,0 +1,11 @@
+package S03_Bit_Magic;
+
+public class gameWithNumber {
+    public static int[] game_with_number(int arr[], int n) {
+        // Complete the function
+        for(int i =0; i<n-1 ; i++){
+            arr[i] = arr[i] ^ arr[i+1];
+        }
+        return arr;
+    }
+}
