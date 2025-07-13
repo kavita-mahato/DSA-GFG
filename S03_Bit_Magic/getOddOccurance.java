db7faa77e74011433e6fe6fdc269cf4a0c28924a
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-13 21:34:12 +0530

    One odd Occuring

---
+++ S03_Bit_Magic/getOddOccurance.java
@@ -0,0 +1,13 @@
+package S03_Bit_Magic;
+
+public class getOddOccurance {
+    int getOddOccurrence(int[] arr) {
+        // code here
+        int n = arr.length;
+        int res = 0;
+        for(int i = 0; i<n; i++){
+            res = res ^ arr[i];
+        }
+        return res;
+    }
+}
