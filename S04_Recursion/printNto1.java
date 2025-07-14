cd995c794b74e0b68dd5c7f8560b6c2530ebd50e
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-14 13:27:17 +0530

    Print n to 1

---
+++ S04_Recursion/printNto1.java
@@ -0,0 +1,21 @@
+/*
+Print numbers from N to 1 (space separated) without the help of loops.
+*/
+
+package S04_Recursion;
+
+public class PrintNto1 {
+    static void printNos(int N) {
+        // code here
+        if (N == 0) {
+            return;
+        }
+        System.out.print(N + " ");
+        printNos(N - 1);
+    }
+
+    public static void main(String[] args) {
+        int N = 10;
+        printNos(N);
+    }
+}
