c4b021b68fbe0c3bb5645041e06d418a89088387
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-14 21:36:56 +0530

    Power Using Recursion

---
+++ S04_Recursion/power.java
@@ -0,0 +1,16 @@
+package S04_Recursion;
+
+public class power {
+    static int RecursivePower(int n, int p) {
+        // Code here
+        if (p == 0) return 1; 
+
+        int halfPower = RecursivePower(n, p / 2);
+
+        if (p % 2 == 0) {
+            return halfPower * halfPower;  
+        }else {
+            return n * halfPower * halfPower; 
+        }
+    }
+}
