773137fcb699d91553a3d6dc8ac0218f74e501c2
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-14 21:43:04 +0530

    Sum of Digits of a Number

---
+++ S04_Recursion/sumDigits.java
@@ -0,0 +1,9 @@
+package S04_Recursion;
+
+public class sumDigits {
+    public static int sumOfDigits(int n) {
+        // Code here
+        if (n == 0) return 0;
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+}
