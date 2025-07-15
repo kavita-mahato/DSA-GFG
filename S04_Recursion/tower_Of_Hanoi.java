0289207931d721be92b89033182ad52a29e85e13
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-15 17:33:31 +0530

    Tower Of Hanoi

---
+++ S04_Recursion/tower_Of_Hanoi.java
@@ -0,0 +1,16 @@
+package S04_Recursion;
+
+public class tower_Of_Hanoi {
+    int count = 0;
+    public int towerOfHanoi(int n, int from, int to, int aux) {
+        // code here
+        if(n==1){
+            count++;
+            return count;
+        }
+        towerOfHanoi(n-1,from,aux,to);
+        count++;
+        towerOfHanoi(n-1,aux,to,from);
+        return count;
+    }
+}
