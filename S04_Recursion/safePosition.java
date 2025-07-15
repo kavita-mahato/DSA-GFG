031d69b833a2a759cccc319606f9d564f464fe3d
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-15 17:43:01 +0530

    Find the Safe Position

---
+++ S04_Recursion/safePosition.java
@@ -0,0 +1,12 @@
+package S04_Recursion;
+
+public class safePosition {
+    public int safePos(int n, int k) {
+        // code here
+        return josephus(n, k) + 1;
+    }
+    private int josephus(int n, int k) {
+        if (n == 1) return 0;
+        return (josephus(n - 1, k) + k) % n;
+    }
+}
