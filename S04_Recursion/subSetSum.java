ede01324f666464534a4665f0af68b73880ceb25
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-15 17:22:26 +0530

    Subset Sums

---
+++ S04_Recursion/subSetSum.java
@@ -0,0 +1,25 @@
+package S04_Recursion;
+
+import java.util.ArrayList;
+
+public class subSetSum {
+    public ArrayList<Integer> subsetSums(int[] arr) {
+        // code here
+        ArrayList<Integer> sumSet = new ArrayList<>();
+        setElements(arr, 0, 0, sumSet);
+        return sumSet;
+    }
+
+    private void setElements(int[] arr, int index, int currentSum, ArrayList<Integer> sumSet) {
+        if (index == arr.length) {
+            sumSet.add(currentSum);
+            return;
+        }
+
+        // Include current element
+        setElements(arr, index + 1, currentSum + arr[index], sumSet);
+
+        // Exclude current element
+        setElements(arr, index + 1, currentSum, sumSet);
+    }
+}
