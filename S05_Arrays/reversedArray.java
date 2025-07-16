85058443f30a74c8cf017c71354b324aef4d0bec
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-16 13:35:04 +0530

    Reverse an Array

---
+++ S05_Arrays/reversedArray.java
@@ -0,0 +1,16 @@
+package S05_Arrays;
+
+public class reversedArray {
+    public void reverseArray(int arr[]) {
+        // code here
+        int n = arr.length;
+        int low=0, high=n-1;
+        while(low < high){
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
