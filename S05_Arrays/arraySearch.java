b515d5bf9e1b8941f26d76012f783cda1001f024
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-16 13:32:39 +0530

    Array Search

---
+++ S05_Arrays/arraySearch.java
@@ -0,0 +1,12 @@
+package S05_Arrays;
+
+public class arraySearch {
+    static int search(int arr[], int x) {
+        // code here
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==x)
+                return i;
+        }
+        return -1;
+    }
+}
