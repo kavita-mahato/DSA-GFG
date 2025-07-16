8b3d3de527f0a0b92e5663c67155ad518049d3a3
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-16 13:27:30 +0530

    Largest Element in Array

---
+++ S05_Arrays/largestNum.java
@@ -0,0 +1,13 @@
+package S05_Arrays;
+
+public class largestNum {
+    public static int largest(int[] arr) {
+        // code here
+        int res =0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>arr[res])
+                res = i;
+        }
+        return arr[res];
+    }
+}
