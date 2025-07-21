71ea8a13d741bf0385dbf7acb79dfc84980c21b8
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-21 22:40:38 +0530

    Print Pattern

---
+++ S04_Recursion/enhancedPrintPattern.java
@@ -0,0 +1,25 @@
+package S04_Recursion;
+
+import java.util.ArrayList;
+
+public class enhancedPrintPattern {
+    public ArrayList<Integer> pattern(int n) {
+        // code here
+        ArrayList<Integer> pattern = new ArrayList<>();
+        generatePattern(n, false, pattern);
+        return pattern;
+
+    }
+    private void generatePattern(int current, boolean increasing, ArrayList<Integer> pattern) {
+        pattern.add(current);
+
+        if (current <= 0) {
+            increasing = true;
+        }
+
+        int next = current + (increasing ? 5 : -5);
+        if (!(increasing && next > pattern.get(0))) { 
+            generatePattern(next, increasing, pattern);
+        }
+    }
+}
