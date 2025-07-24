6ae304ffd1723f9fdebe7938f5f989aaf28dbc45
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-24 23:04:09 +0530

    Play with OR

--- S05_Arrays/gameWithNum.java
+++ S05_Arrays/gameWithNum.java
@@ -1,3 +1,8 @@
+/* Play With OR 
+You are given an array arr[] of length n, you have to re-construct the same array arr[] in-place. 
+The arr[i] after reconstruction will become arr[i] OR arr[i+1], where OR is bitwise or. If for some i, 
+i+1 does not exists, then do not change arr[i]. */
+
 public class gameWithNum {
     public static int[] game_with_number(int arr[], int n) {
         // Complete the function
