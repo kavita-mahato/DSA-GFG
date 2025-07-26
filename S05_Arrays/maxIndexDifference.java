10f9d7424423bb10f8202bdc9d8db0d196794928
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-26 19:54:15 +0530

    Maximum Index

--- S05_Arrays/maxIndexDifference.java
+++ S05_Arrays/maxIndexDifference.java
@@ -1,15 +1,31 @@
 public class maxIndexDifference {
     int maxIndexDiff(int arr[]) {
         // code here
-        int result = 0 ;
         int n = arr.length;
-        
-        for(int i = 0; i<n-1; i++){
-            for(int j = i; j<n; j++){
-                if(arr[i] <= arr[j]) 
-                result = Math.max(result,(j-i));
+
+        // Step 1: Create LMin[] to store min from left
+        int[] LMin = new int[n];
+        LMin[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            LMin[i] = Math.min(arr[i], LMin[i - 1]);
+        }
+        // Step 2: Create RMax[] to store max from right
+        int[] RMax = new int[n];
+        RMax[n - 1] = arr[n - 1];
+        for (int j = n - 2; j >= 0; j--) {
+            RMax[j] = Math.max(arr[j], RMax[j + 1]);
+        }
+        // Step 3: Traverse LMin and RMax to find maximum j - i
+        int i = 0, j = 0, maxDiff = -1;
+        while (i < n && j < n) {
+            if (LMin[i] <= RMax[j]) {
+                maxDiff = Math.max(maxDiff, j - i);
+                j++;
+            } else {
+                i++;
             }
         }
-        return result;
+
+        return maxDiff;
     }
 }
