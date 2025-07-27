229dcb98ff214d314947120ab4c38052122345ec
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-27 19:05:37 +0530

    Swap odd and even bits

--- S03_Bit_Magic/swapEvenOddBits.java
+++ S03_Bit_Magic/swapEvenOddBits.java
@@ -1,8 +1,12 @@
+/* Given an unsigned integer n, You have  to swap all even-position bits with their rightside adjacent odd-position bits. */
+
 package S03_Bit_Magic;
 
 public class swapEvenOddBits {
     public static int swapBits(int n) {
         // code here
+        /*
+        // Naive solution
         String binary = Integer.toBinaryString(n);
 
         // Pad with leading zeros to make length even
@@ -22,5 +26,22 @@ public class swapEvenOddBits {
         // Convert back to integer
         String swappedBinary = new String(bits);
         return Integer.parseInt(swappedBinary, 2);
+        */
+
+        // Optimized solution
+        int result = 0;
+        int totalBits = Integer.SIZE;
+
+        for (int i = 0; i < totalBits; i += 2) {
+            // Extract bit at even position i
+            int evenBit = (n >> i) & 1;
+            // Extract bit at odd position i+1
+            int oddBit = (n >> (i + 1)) & 1;
+
+            // Swap and place the bits
+            result |= (oddBit << i); // Put oddBit in the even position i
+            result |= (evenBit << (i + 1)); // Put evenBit in the odd position i+1
+        }
+        return result;
     }
 }
