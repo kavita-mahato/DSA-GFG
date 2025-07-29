a5018cf510d047be1006653f353f46b02f82702a
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-29 20:56:40 +0530

    Longest Consecutive

--- S03_Bit_Magic/longestConsecutive.java
+++ S03_Bit_Magic/longestConsecutive.java
@@ -3,10 +3,11 @@ package S03_Bit_Magic;
 public class longestConsecutive {
     public static int maxConsecutiveOnes(int n) {
         // code here
+        /*
         String binary = Integer.toBinaryString(n);  // Convert integer to binary string
         int maxCount = 0;
         int currentCount = 0;
-
+        
         for (char bit : binary.toCharArray()) {
             if (bit == '1') {
                 currentCount++;
@@ -16,5 +17,21 @@ public class longestConsecutive {
             }
         }
         return maxCount;        
+        */
+
+        // Enhanced approach
+        int maxCount = 0;
+        int currentCount = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+            n = n >> 1;
+        }
+        return maxCount;
     }
 }
