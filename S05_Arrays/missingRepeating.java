d17db85ac5adc4be5174633675d214844ca60e34
Kavita Mahato <kavitamahato0835@gmail.com>
2025-08-06 18:42:20 +0530

    Missing And Repeating

---
+++ S05_Arrays/missingRepeating.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class missingRepeating {
+    public boolean checkDuplicatesWithinK(int[] arr, int k) {
+        // your code
+        ArrayList<Integer> set = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            // If current element exists in k distance
+            if (set.contains(arr[i])) {
+                return true;
+            }
+
+            // Add current element to the set
+            set.add(arr[i]);
+
+            // Maintain set size = k
+            if (set.size() > k) {
+                set.remove(0);  // Remove the index=0 element
+            }
+        }
+
+        return false;
+    }
+}
