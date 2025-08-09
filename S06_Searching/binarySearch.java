0274d5a99fb970e67b09bb6d8d2b6eeaa422df69
Kavita Mahato <kavitamahato0835@gmail.com>
2025-08-09 21:17:19 +0530

    Sorted Array Search

---
+++ S06_Searching/binarySearch.java
@@ -0,0 +1,16 @@
+public class binarySearch {
+    static boolean searchInSorted(int arr[], int k) {
+        // Your code here
+        int n = arr.length;
+        int low = 0, high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] == k) return true;
+            else if(arr[mid] > k)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return false;
+    }
+}
