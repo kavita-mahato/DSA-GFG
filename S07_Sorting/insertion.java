2c46ccea57f01333d21c264a24123f37d1dd83d9
Kavita Mahato <kavitamahato0835@gmail.com>
2025-08-20 19:00:02 +0530

    Insertion Sort

---
+++ S07_Sorting/insertion.java
@@ -0,0 +1,17 @@
+package S07_Sorting;
+
+public class insertion {
+    public void insertionSort(int arr[]) {
+        // code here
+        int n = arr.length;
+        for(int i=1; i<n; i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+}
