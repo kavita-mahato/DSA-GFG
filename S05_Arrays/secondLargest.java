a946be6a4ffc30c4f54dd88438ffe0f9d7099724
Kavita Mahato <kavitamahato0835@gmail.com>
2025-11-27 17:53:42 +0530

    Second largest element in an array

--- S05_Arrays/secondLargest.java
+++ S05_Arrays/secondLargest.java
@@ -1,6 +1,20 @@
 public class secondLargest {
     public int getSecondLargest(int[] arr) {
         // code here
+        int n = arr.length;
+        int largest = arr[0];
+        int slargest = -1;
+        for(int i=1; i<n; i++){
+            if(largest < arr[i]){
+                slargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > slargest){
+                slargest = arr[i];
+            }
+        }
+        return slargest;
+        /* 
         int res=-1, largest=0;
         for(int i=1; i<arr.length; i++){
             if(arr[i]>arr[largest]){
@@ -8,9 +22,10 @@ public class secondLargest {
                 largest=i;
             }else if(arr[i] != arr[largest]){
                 if(res == -1 || arr[i] > arr[res])
-                    res = i;
+                res = i;
             }
         }
         return res == -1 ? -1 : arr[res];
+        */
     }
 }
